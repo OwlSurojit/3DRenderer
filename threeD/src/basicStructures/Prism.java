609752e70ad8b81e3bc5a0c6e4ae1f409ef1f221
OwlSurojit <surojit@gmx.de>
2018-07-04 13:43:06 +0200

    Added a Prism class that includes only a commented constructor.

---
+++ threeD/src/basicStructures/Prism.java
@@ -0,0 +1,23 @@
+package basicStructures;
+
+import mainRenderer.Structure;
+
+public class Prism extends Structure {
+	
+	Point[] vertices;
+	
+	public Prism(Point baseCentre, int baseVertices, int radius, int height) {
+		/*
+		 * Defines a Prism with an equilateral base of int baseVertices (2nd arg) vertices.
+		 * Its centre is the Point baseCentre (1st arg).
+		 * Its radius is int radius (3rd arg).
+		 * The height of the prism is defined with int height (4th arg).
+		 */
+	}
+
+	@Override
+	public void displayEdges() {
+		
+	}
+
+}
